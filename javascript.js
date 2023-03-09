16be9c0d6c0498c2ac4a1f4b4b3c61d6edec92c6
KevinHaraux <kevin.haraux@gmail.com>
2023-03-09 16:37:24 +0100

    01

--- javascript.js
+++ javascript.js
@@ -29,7 +29,8 @@ function chercheAdresse(valeur)
             retour+= '<ul>';
             let element = document.getElementById('result');
             element.innerHTML = retour;
-            element.style.background = "#fff";
+            /*element.style.display = 'block';
+            element.style.background = "#fff";*/
         }
         else {
             console.log('Requête échouée');
